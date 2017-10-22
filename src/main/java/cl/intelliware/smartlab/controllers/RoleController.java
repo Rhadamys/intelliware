4f27a3aaff7fd5d2f41016c3d30165c6cbd2c580
Javier <javierkns@gmail.com>
2017-10-22 18:23:23 -0300

    added API with gets (v1.0)

---
+++ src/main/java/cl/intelliware/smartlab/controllers/RoleController.java
@@ -0,0 +1,35 @@
+package cl.intelliware.smartlab.controllers;
+
+import cl.intelliware.smartlab.models.Role;
+import cl.intelliware.smartlab.repositories.RoleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/role")
+public class RoleController
+{
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleController(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Role> getAllRoles()
+    {
+        return roleRepository.findAll();
+    }
+
+    @GetMapping(path="/{id}")
+    public @ResponseBody Role getRole(@PathVariable("id") Integer id)
+    {
+        System.out.println(roleRepository);
+        long lid = id.longValue();
+        return roleRepository.findOne(lid);
+    }
+}
---
+++ src/main/java/cl/intelliware/smartlab/controllers/StudentController.java
@@ -0,0 +1,35 @@
+package cl.intelliware.smartlab.controllers;
+
+import cl.intelliware.smartlab.models.Student;
+import cl.intelliware.smartlab.repositories.StudentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/student")
+public class StudentController
+{
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Student> getAllStudents()
+    {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping(path="/{id}")
+    public @ResponseBody Student getSection(@PathVariable("id") Integer id)
+    {
+        System.out.println(studentRepository);
+        long lid = id.longValue();
+        return studentRepository.findOne(lid);
+    }
+}
