c8509c6f50ecb9f057351b100537ef6de0e07a91
unknown <natalia.guzman.s@usach.cl>
2017-10-31 18:41:21 -0300

    logout test

--- src/main/resources/static/js/controllers/main-controller.js
+++ src/main/resources/static/js/controllers/main-controller.js
@@ -4,4 +4,28 @@ app.controller('MainController', ['$scope', '$location', function($scope, $locat
 	$scope.isActive = function(route) {
         return route === $location.path();
     }
+    $scope.logout = function() {
+        logout($scope.loginData);
+    }
+    function logout(params) {
+        var req = {
+            method: 'DELETE',
+            url: "google/login"
+        }
+        $http(req).then(
+            function(data){
+                $cookies.remove("access_token");
+                window.location.href="login";
+            },function(){
+                console.log("error");
+            }
+        );
+    }
+
+    function logout2(){
+	    var auth2 = gapi.auth2.getAuthInstance();
+	    auth2,signOut().then(function () {
+	        console.log('user signer out.')
+        })
+    }
 }]);
